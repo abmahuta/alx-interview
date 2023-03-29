ceba788a361e68e951dc6684a93cec916be45571
Abdulkadir Badamasi <mahuta2012@gmail.com>
2023-03-29 20:07:36 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''A program that checks if all lockedboxes can be unlocked.
+'''
+
+def canUnlockAll(boxes):
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = [0]
+    unseen_boxes = boxes[0][:]
+    while unseen_boxes:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes.extend(boxes[boxIdx])
+            seen_boxes.append(boxIdx)
+    return n == len(seen_boxes)
+
