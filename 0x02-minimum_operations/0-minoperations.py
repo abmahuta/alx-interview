49083ae42f77d971e2cd992b5cdb30036fc09e1d
Abdulkadir Badamasi <mahuta2012@gmail.com>
2023-04-07 06:59:18 +0000

    refactor code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,28 +7,16 @@ def minOperations(n):
     '''Calculates the fewest number of operations needed to result
     in exactly n H characters.
     '''
-    if not isinstance(n, int):
+def minOperations(n):
+    if n == 1:
         return 0
-    opr_count = 0
-    clipboard = 0
-    char_count = 1
+    ops = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            ops += i
+            n //= i
+        else:
+            i += 1
+    return ops
 
-    while char_count < n:
-        if clipboard < char_count:
-
-            clipboard = char_count
-            char_count += clipboard
-            opr_count += 2
-
-        elif n - char_count > 0 and (n - char_count) % char_count == 0:
-
-            clipboard = char_count
-            char_count += clipboard
-            opr_count += 2
-
-        elif clipboard > 0:
-            char_count += clipboard
-            opr_count += 1
-    
-
-    return opr_count
