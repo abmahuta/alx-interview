716ac08dff9549eedebc5ba9c492e71ea8a25b35
Abmahuta <mahuta2012@gmail.com>
2023-05-04 22:54:48 +0100

    starwars correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,47 +1,25 @@
 #!/usr/bin/node
-const https = require('https');
-
+const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  https.get(`${API_URL}/films/${process.argv[2]}/`, (res) => {
-    let data = '';
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    res.on('end', () => {
-      const charactersURL = JSON.parse(data).characters;
-      const charactersName = charactersURL.map(
-        (url) =>
-          new Promise((resolve, reject) => {
-            https.get(url, (res) => {
-              let data = '';
-
-              res.on('data', (chunk) => {
-                data += chunk;
-              });
-
-              res.on('end', () => {
-                resolve(JSON.parse(data).name);
-              });
-
-              res.on('error', (err) => {
-                reject(err);
-              });
-            });
-          })
-      );
-
-      Promise.all(charactersName)
-        .then((names) => console.log(names.join('\n')))
-        .catch((allErr) => console.error(allErr));
-    });
-
-    res.on('error', (err) => {
-      console.error(err);
-    });
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
-
